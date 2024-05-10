12812eb3113c56e7d3abca114a471b45573c49e5
MartinLzarov <martinlzarov@gmail.com>
2024-05-10 13:21:33 -0700

    update font size

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,10 +6,10 @@ export default function Home() {
 
     return (
         <main className="bg-[url('/assets/images/wire-circles.png')] bg-cover bg-center">
-            <div className="flex min-h-screen flex-col items-center !mt-8 container xl:flex-row xl:mt-0 gap-y-10">
+            <div className="flex min-h-screen flex-col items-center !mt-8 container xl:flex-row xl:mt-0 gap-y-10 sm:max-w-full">
             <div className="basis-3/5">
                 <h1 className="uppercase mb-4 text-3xl xl:text-5xl">Revolutionizing Wireless Charger</h1>
-                <p className="mb-7 max-w-lg font-bold">
+                <p className="mb-7 max-w-lg font-bold text-base xl:text-lg">
                 Wireless charging device with interactive features revolutionizing the restaurant industry. By combining convenience, comfort and modern technologies, the product aims to create more favorable conditions for customer service. From food ordering, integrated split payments and feedback form in our web application we tend to grow with the establishment, which places our device. We provide valuable data with it and help your restaurant to be more frequently visited.
                 </p>
                 <a href="/en/technology" className="bg-chargeMeYellow py-4 px-5 w-max block font-bold rounded-md">ChargeMe</a>
